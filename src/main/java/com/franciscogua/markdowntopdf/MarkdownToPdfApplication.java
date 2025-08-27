0dfced37651987f1a13129b0c399d6ca5a9c925b
franciscogua <franguaful@gmail.com>
2025-08-27 09:51:17 -0300

    feat(backend): setup initial Spring Boot structure

---
+++ src/main/java/com/franciscogua/markdowntopdf/MarkdownToPdfApplication.java
@@ -0,0 +1,13 @@
+package com.franciscogua.markdowntopdf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarkdownToPdfApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MarkdownToPdfApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/franciscogua/markdowntopdf/service/PdfService.java
@@ -0,0 +1,11 @@
+package com.franciscogua.markdowntopdf.service;
+
+import com.franciscogua.markdowntopdf.dto.MarkdownRequestDto;
+import org.springframework.stereotype.Service;
+
+@Service
+public class PdfService {
+    public void generatePdf(MarkdownRequestDto requestDto) {
+        
+    }
+}
